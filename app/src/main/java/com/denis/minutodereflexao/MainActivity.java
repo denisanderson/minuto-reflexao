dcd806713e5356afff62ac8bdeb7b80443a9b20e
Denis Ribeiro <denisranderson@gmail.com>
2017-07-30 17:34:27 -0300

    Aplica efeito pressionado no botão de sorteio de nova mensagem

--- app/src/main/java/com/denis/minutodereflexao/MainActivity.java
+++ app/src/main/java/com/denis/minutodereflexao/MainActivity.java
@@ -1,13 +1,28 @@
 package com.denis.minutodereflexao;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Constante para uso nas mensagens de log para facilitar localização
+    public final static String LOG_TAG = "MR_App";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton btnSorteia = (FloatingActionButton) findViewById(R.id.fab_sorteia);
+        btnSorteia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Nova mensagem", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
