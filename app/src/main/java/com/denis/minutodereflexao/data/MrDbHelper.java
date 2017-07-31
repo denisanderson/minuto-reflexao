57332375a28470d6bdca1c78a8778cab846d7ee1
Denis Ribeiro <denisranderson@gmail.com>
2017-07-31 17:02:31 -0300

    Cria classe MrDbHelper

---
+++ app/src/main/java/com/denis/minutodereflexao/data/MrDbHelper.java
@@ -0,0 +1,49 @@
+package com.denis.minutodereflexao.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MrDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "mr.db";
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * Create a helper object to create, open, and/or manage a database.
+     *
+     * @param context to use to open or create the database
+     * @param name    of the database file, or null for an in-memory database
+     * @param factory to use for creating cursor objects, or null for the default
+     * @param version number of the database (starting at 1); if the database is older,
+     *                {@link #onUpgrade} will be used to upgrade the database; if the database is
+     *                newer, {@link #onDowngrade} will be used to downgrade the database
+     */
+    public MrDbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * Called when the database is created for the first time.
+     *
+     * @param db The database.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    /**
+     * Called when the database needs to be upgraded. The implementation
+     * should use this method to drop tables, add tables, or do anything else it
+     * needs to upgrade to the new schema version.
+     *
+     * @param db         The database.
+     * @param oldVersion The old database version.
+     * @param newVersion The new database version.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
