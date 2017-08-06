e8fc0af8288e01012a2c0bd086a6537ed13cab4c
Denis Ribeiro <denisranderson@gmail.com>
2017-08-06 14:32:40 -0300

    Implementa lógica de atualização do banco de dados local usando SharedPreferences

--- app/src/main/java/com/denis/minutodereflexao/MainActivity.java
+++ app/src/main/java/com/denis/minutodereflexao/MainActivity.java
@@ -1,5 +1,7 @@
 package com.denis.minutodereflexao;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.support.design.widget.FloatingActionButton;
@@ -16,9 +18,8 @@ import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Constante para uso nas mensagens de log para facilitar localização
-    private final static String LOG_TAG = "MainActivity";
-
+    public static final String PREFS_FILE = "MRPrefs";
+    private static final String LOG_TAG = "MainActivity";
     TextView mTxtTitulo;
     TextView mTxtTexto;
     TextView mTxtAutor;
@@ -43,52 +44,81 @@ public class MainActivity extends AppCompatActivity {
         mTxtTexto = (TextView) findViewById(R.id.txt_texto);
         mTxtAutor = (TextView) findViewById(R.id.txt_autor);
 
+        // Inicializa variavel auxiliar para evitar repetição da mensagem
         intIdAnterior = 0;
 
-        apagaDatabase();
+        // Eecuta rotina para verificar se o banco de dados do app
+        // precisa ser atualizado no aparelho
+        verificaDbUpdate();
+
     }
 
+    /**
+     * Verifica se a versão do banco de dados informado no App
+     * é o mesmo registrado no SharedPreferences no aparelho
+     * Se diferente, apaga o DB local para permitir cópia da nova versão
+     */
+    private void verificaDbUpdate() {
+        SharedPreferences sharedPref = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        int dbVersion = sharedPref.getInt(getString(R.string.shared_prefs_db_version), 0); // Retorna 0 se o valor não existir
+        if (dbVersion != DbHelper.DATABASE_VERSION) {
+            Log.i(LOG_TAG, "PREFS. " + dbVersion + " DATABASE_VERSION " + DbHelper.DATABASE_VERSION);
+            apagaDatabase();
+            atualizaSharedPrefs(sharedPref,DbHelper.DATABASE_VERSION);
+        }
+    }
+
+    /**
+     * Atualiza SharedPreferences com o valor da versão atual do banco de dados
+     *
+     * @param sharedPref Objeto sharedPreferences aberto para leitura
+     * @param databaseVersion Versão do banco de dados para escrever na Preferencia
+     */
+    private void atualizaSharedPrefs(SharedPreferences sharedPref, int databaseVersion) {
+        SharedPreferences.Editor mEditor= sharedPref.edit();
+        mEditor.putInt(getString(R.string.shared_prefs_db_version), databaseVersion);
+        mEditor.commit();
+    }
+
+    /**
+     * Se o arquivo de banco de dados existir, o apaga.
+     */
     private void apagaDatabase() {
         DbAccess mDbAccess = DbAccess.getInstance(this);
         if (mDbAccess.checkDataBase()) {
-            Log.i(LOG_TAG, "DB encontrado. Apagando arquivo...");
+            Log.i(LOG_TAG, "DB encontrado. Apagando arquivo.");
             try {
                 this.deleteDatabase(DbHelper.DATABASE_NAME);
             } catch (SQLException e) {
                 Log.i(LOG_TAG, e.getMessage());
             }
         } else {
-            Log.i(LOG_TAG, "Banco de dados não encontrado");
+            Log.i(LOG_TAG, "Arquivo de banco de dados não encontrado");
         }
     }
 
     private void sorteiaMensagem() {
-
         DbAccess mDbAccess = DbAccess.getInstance(this);
-        Log.i(LOG_TAG, "Conecta no banco");
         mDbAccess.openRead();
 
         Log.i(LOG_TAG, "Executa getMensagemAleatoria()");
         mCursor = mDbAccess.getMensagemAleatoria();
 
         mCursor.moveToFirst();
-        Log.i(LOG_TAG, "ID Anterior: " + intIdAnterior + " - ID Atual: " + mCursor.getInt(mCursor.getColumnIndex(DbAccess.COLUNA_ID)));
+        Log.i(LOG_TAG, "ID Anterior " + intIdAnterior + " ID Atual " + mCursor.getInt(mCursor.getColumnIndex(DbAccess.COLUNA_ID)));
         int intIdAtual = mCursor.getInt(mCursor.getColumnIndex(DbAccess.COLUNA_ID));
         if (intIdAtual == intIdAnterior) {
-            Log.i(LOG_TAG, "Executa  Novamente getMensagemAleatoria()");
+            Log.i(LOG_TAG, "Re-executa getMensagemAleatoria()");
             mCursor = mDbAccess.getMensagemAleatoria();
             mCursor.moveToFirst();
         }
 
         // Coloca o resultado na tela
-        Log.i(LOG_TAG, "Titulo: " + mCursor.getString(mCursor.getColumnIndex(DbAccess.COLUNA_TITULO)));
         mTxtTitulo.setText(mCursor.getString(mCursor.getColumnIndex(DbAccess.COLUNA_TITULO)));
         mTxtTexto.setText(mCursor.getString(mCursor.getColumnIndex(DbAccess.COLUNA_TEXTO)));
         mTxtAutor.setText(mCursor.getString(mCursor.getColumnIndex(DbAccess.COLUNA_AUTOR)));
         intIdAnterior = intIdAtual;
 
-        Log.i(LOG_TAG, "Fecha conexão com o banco");
         mDbAccess.close();
-
     }
 }
